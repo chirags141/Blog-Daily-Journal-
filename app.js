1cfc3a36825a19de99be085b0bd486ce27ae545a
Chirag <chiragsethiya123@gmail.com>
2020-04-29 22:07:18 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get("/posts/:postId", function(req, res){
     });
   })
 
-  const port = process.env.PORT
-app.listen(3000, function() {
+  const port = process.env.PORT || 3000
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
